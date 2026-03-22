9438a90def53673c961c91178b829ec98a5fb2ef
useon <roqkftjs@gmail.com>
2026-03-22 21:24:21 +0900

    refactor: 회의실 예약 페이지 구현을 feature 폴더 내부로 이동

--- src/pages/Routes.tsx
+++ src/pages/Routes.tsx
@@ -1,5 +1,5 @@
-import { ReservationStatusPage } from './ReservationStatusPage';
-import { RoomBookingPage } from './RoomBookingPage';
+import { ReservationStatusPage } from 'features/meeting-room-reservation/pages/ReservationStatusPage';
+import { RoomBookingPage } from 'features/meeting-room-reservation/pages/RoomBookingPage';
 import { Route, Routes as ReactRouterRoutes, Navigate } from 'react-router-dom';
 
 export const Routes = () => {
