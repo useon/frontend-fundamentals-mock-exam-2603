005c7d13cf903222bcc718b81d3fecbfbab2ded0
useon <roqkftjs@gmail.com>
2026-03-22 20:42:23 +0900

    refactor: 회의실 예약 feature 구조와 alias 추가

--- vite.config.ts
+++ vite.config.ts
@@ -33,6 +33,7 @@ export default defineConfig({
       utils: path.resolve(__dirname, 'src/utils'),
       hooks: path.resolve(__dirname, 'src/hooks'),
       stores: path.resolve(__dirname, 'src/stores'),
+      features: path.resolve(__dirname, 'src/features'),
     },
   },
   server: {
